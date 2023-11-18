4b6823f02d883fa47ffd576726897a8daa44ca53
Glitch (glitch-blank-website) <none>
2023-11-18 10:16:56 +0000

    🏁🍶 Checkpoint ./style.css:63026973/413 ./system.js:63026973/9080 ./index.html:63026973/19703 ./script.js:63026973/659

---
+++ script.js
@@ -0,0 +1,157 @@
+const header = document.getElementById("mainHeader");
+function fullscreen() {
+  document.documentElement.requestFullscreen();
+} // 全画面モードにする
+
+function info() {
+  alert(
+    "PRODUCTION(制作):Jamstec/Hayatos/xxx\nUPDATELOG:1.0-作成 1.01-難読化 1.1-プルタブ追加\n1.11-UI調整 1.12-UI自動最適化仮実装 1.13-リンク追加\n1.14-微調整 1.15-リリース 1.16-info及びリンク追加\n1.17-新しいタブ試験実装　1.18-タブ数6→8 1.19-ページ操作\n1.20-自由リンク 1.2i-アイコン追加 1.22-URLバー,リロード追加"
+  );
+}
+
+function help() {
+  alert(
+    "無限の可能性を秘めているproxy\njavascriptを有効にしてください。\npcサイズの画面で正常に機能します。\nOpenNewWindowで大画面表示します。\nproxy更新及びリンクのは(proxy寄付)ボタンから。"
+  );
+}
+
+$("#text4")
+  .focus(function () {
+    $(".password_box").animate(
+      {
+        borderTopColor: "#3be5ae",
+        borderLeftColor: "#3be5ae",
+        borderRightColor: "#3be5ae",
+        borderBottomColor: "#3be5ae",
+      },
+      200
+    );
+  })
+  .blur(function () {
+    $(".password_box").animate(
+      {
+        borderTopColor: "＃191970",
+        borderLeftColor: "＃191970",
+        borderRightColor: "＃191970",
+        borderBottomColor: "＃191970",
+      },
+      200
+    );
+  });
+
+let text4 = document.getElementById("text4");
+let urlvar = "0"
+let UB = document.getelementById("UB");
+
+text4.addEventListener("keypress", function (e) {
+  if (e.keyCode === 13) {
+    urlvar = document.getElementById("text4").value;
+    if (urlvar) {
+      if (urlvar.indexOf("https://") === -1) {
+        URL = "https://" + urlvar;
+      } else {
+        URL = urlvar;
+      }
+      if (number === foremost) {
+        if (URL !== urls[foremost]) {
+          number++;
+          foremost++;
+          urls.push(URL);
+        }
+        console.log(urls);
+        console.log(number);
+        console.log(foremost);
+        document.getElementById("text4").value = URL;
+        UB.location = URL;
+      } else {
+        if (URL !== urls[foremost]) {
+          //
+          foremost++;
+          urls.push(URL);
+        }
+        number = foremost;
+        console.log(urls);
+        console.log(number);
+        console.log(foremost);
+        document.getElementById("text4").value = URL;
+        UB.location = URL;
+      }
+    } else {
+      console.log("error"); //削除
+    }
+  }
+});
+
+let number = -1;
+let foremost = -1;
+var urls = [];
+var URL = 0;
+
+function link(URL) {
+  //クリック時
+  if (URL) {
+    if (number === foremost) {
+      //最先端か確認
+      if (URL !== urls[foremost]) {
+        number++;
+        foremost++;
+        urls.push(URL);
+      }
+      console.log(urls);
+      console.log(number);
+      console.log(foremost);
+      document.getElementById("text4").value = URL;
+      UB.location = URL;
+    } else {
+      if (URL !== urls[foremost]) {
+        //最先端か確認
+        foremost++; //重複リンクは表示しない
+        urls.push(URL);
+      }
+      number = foremost; //最先端の位置に設定
+      console.log(urls);
+      console.log(number);
+      console.log(foremost);
+      console.log("ok");
+      document.getElementById("text4").value = URL;
+      UB.location = URL;
+    }
+  }
+}
+
+function go() {
+  //前
+  if (number < foremost) {
+    number++;
+    URL = urls[number];
+    console.log(number);
+    console.log(foremost);
+    document.getElementById("text4").value = URL;
+    UB.location = URL;
+  }
+}
+
+function back() {
+  //戻る
+  if (number > 0) {
+    number--;
+    URL = urls[number];
+    console.log(number);
+    console.log(foremost);
+    document.getElementById("text4").value = URL;
+    UB.location = URL;
+  }
+}
+
+function history() {
+  alert(urls);
+}
+
+function reload() {
+  if (foremost > "-1") {
+    URL = urls[number];
+    console.log(number);
+    console.log(foremost);
+    UB.location = URL;
+  }
+}
