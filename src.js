5a52ae602bcf43a9da9933ce120f3dda482eb59c
Glitch (astro-network) <none>
2023-11-26 11:54:17 +0000

    🌎🍉 Checkpoint ./src.js:63026973/557 ./index.html:63026973/50

--- src.js
+++ src.js
@@ -7,12 +7,12 @@ function app(appname) {
   alert(appname);
   switch (appname) {
     case "browser":
+      alert(document.getElementById('script'))
+      document.getElementById('script').innerHTML = '<script src="./src/browser/browser.js"></script>'
       let response = fetch("./src/browser/browser.html")
       .then(response => response.text())
       .then(data => {
-        console.log(data)
         if (leftacsses === "0"){
-          
           document.getElementById('leftwindow').innerHTML = data
           leftacsses++;
           alert(leftacsses + "leftacsses");
